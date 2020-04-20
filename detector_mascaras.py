66f5b21954de37c5f549660582098c0b110c2b4e
Weslley Leocadio <43214589+WesleyLeocadio@users.noreply.github.com>
2020-04-20 12:16:23 -0300

    Add files via upload

---
+++ detector_mascaras.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+
+# In[35]:
+
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+
+# Load the model
+model = tensorflow.keras.models.load_model('keras_model.h5')
+
+
+# In[8]:
+
+
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+
+# In[39]:
+
+
+
+# Replace this with the path to your image
+image = Image.open('Image test/image.jpeg')
+
+
+# In[29]:
+
+
+
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+
+# In[30]:
+
+
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+
+# display the resized image
+image.show()
+
+
+# In[31]:
+
+
+
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+
+# In[32]:
+
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+
+# In[33]:
+
+
+
+# run the inference
+prediction = model.predict(data)[0]
+
+
+# In[34]:
+
+
+if prediction[0]>= prediction[1]:
+    print('NO MASK')
+else:
+    print('MASK')
+
+
+# In[ ]:
+
+
+
+
